ad34122ad31252af094978a8dcf6c9ef64ae8d83
Denis Kondratev <kondratev.da@gmail.com>
2024-03-11 01:45:47 +0300

    Refactor the 383 problem solvation

--- solution_383/src/lib.rs
+++ solution_383/src/lib.rs
@@ -24,15 +24,14 @@ pub struct Solution;
 impl Solution {
     pub fn can_construct(ransom_note: String, magazine: String) -> bool {
         let mut map = magazine
-            .as_bytes()
-            .iter()
-            .fold([0; 26], |mut map, &x| {
+            .bytes()
+            .fold([0; 26], |mut map, x| {
                 let i = (x - b'a') as usize;
                 map[i] += 1;
                 map
             });
 
-        for &x in ransom_note.as_bytes().iter() {
+        for x in ransom_note.bytes() {
             let i = (x - b'a') as usize;
             map[i] -= 1;
 
