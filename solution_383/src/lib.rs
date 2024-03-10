7986faf574957ce42bfa1d3bfcb1ba798e6aa68f
Denis Kondratev <kondratev.da@gmail.com>
2024-03-11 01:31:47 +0300

    Solve the 383 problem

--- solution_383/src/lib.rs
+++ solution_383/src/lib.rs
@@ -1,22 +1,46 @@
 /// # [383. Ransom Note](https://leetcode.com/problems/ransom-note/description/?envType=study-plan-v2&envId=top-interview-150)
 ///
-/// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by
-/// using the letters from magazine and false otherwise. Each letter in magazine can only be
-/// used once in ransomNote.
+/// Given two strings `ransomNote` and `magazine`, return `true` if `ransomNote` can be
+/// constructed by using the letters from `magazine` and `false` otherwise. Each letter in
+/// `magazine` can only be used once in `ransomNote`.
 ///
-/// # Example 1:
-/// - Input: ransomNote = "a", magazine = "b"
-/// - Output: false
+/// ## Example 1:
+/// - Input: `ransomNote` = "a", `magazine` = "b"
+/// - Output: `false`
 ///
-/// # Example 2:
-/// - Input: ransomNote = "aa", magazine = "ab"
-/// - Output: false
+/// ## Example 2:
+/// - Input: `ransomNote` = "aa", `magazine` = "ab"
+/// - Output: `false`
 ///
-/// # Example 3:
-/// - Input: ransomNote = "aa", magazine = "aab"
-/// - Output: true
+/// ## Example 3:
+/// - Input: `ransomNote` = "aa", `magazine` = "aab"
+/// - Output: `true`
 ///
-/// # Constraints:
-/// - `1 <= ransomNote.length, magazine.length <= 10<sup>5</sup>`
+/// ## Constraints:
+/// - 1 <= `ransomNote.length`, `magazine.length` <= 10<sup>5</sup>
 /// - `ransomNote` and `magazine` consist of lowercase English letters.
 pub struct Solution;
+
+impl Solution {
+    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        let mut map = magazine
+            .as_bytes()
+            .into_iter()
+            .fold([0; 26], |mut map, &x| {
+                let i = (x - b'a') as usize;
+                map[i] += 1;
+                map
+            });
+
+        for &x in ransom_note.as_bytes().iter() {
+            let i = (x - b'a') as usize;
+            map[i] -= 1;
+
+            if map[i] < 0 {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
