d5c886383e7593f01d760b28bd1bc5861a86dd5b
Denis Kondratev <kondratev.da@gmail.com>
2024-03-10 18:09:27 +0300

    Refactor problems

--- solution_2540/src/lib.rs
+++ solution_2540/src/lib.rs
@@ -4,14 +4,14 @@
 
  Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer
  common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
- Note that an integer is said to be common to nums1 and nums2 if both arrays have at least 
+ Note that an integer is said to be common to nums1 and nums2 if both arrays have at least
  one occurrence of that integer.
- 
+
  Example 1:
    Input: nums1 = [1,2,3], nums2 = [2,4]
    Output: 2
    Explanation: The smallest element common to both arrays is 2, so we return 2.
-   
+
  Example 2:
    Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
    Output: 2
@@ -23,7 +23,7 @@ Constraints:
   Both nums1 and nums2 are sorted in non-decreasing order.
  */
 
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn get_common(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
@@ -50,8 +50,4 @@ impl Solution {
             }
         }
     }
-}
-
-fn main() {
-    println!("Hello, world!");
 }
