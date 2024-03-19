6e0baac643a44d4a43af2d14cad3205047889608
Denis Kondratev <kondratev.da@gmail.com>
2024-03-19 23:56:33 +0300

    Add the 621 problem solution

--- solution_621/src/lib.rs
+++ solution_621/src/lib.rs
@@ -32,3 +32,18 @@
 /// - `tasks[i]` is an uppercase English letter
 /// - 0 <= `n` <= 100
 pub struct Solution;
+
+impl Solution {
+    pub fn least_interval(tasks: Vec<char>, n: i32) -> i32 {
+        const MAP_SIZE:usize = (b'Z' - b'A' + 1) as usize;
+        let mut map = [0i32; MAP_SIZE];
+
+        for &x in tasks.iter() {
+            map[x as usize - b'A' as usize] += 1;
+        }
+
+        let &max_value = map.iter().max().unwrap_or(&0);
+        let max_count = map.iter().filter(|&&x| x == max_value).count() as i32;
+        (tasks.len() as i32).max((max_value - 1) * (n + 1) + max_count)
+    }
+}
