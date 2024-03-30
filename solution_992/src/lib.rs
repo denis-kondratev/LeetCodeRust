eac5f3c9328294caf837dd291c1385f78ff426b1
Denis Kondratev <kondratev.da@gmail.com>
2024-03-31 00:44:41 +0300

    Add the 992 problem solution

--- solution_992/src/lib.rs
+++ solution_992/src/lib.rs
@@ -23,3 +23,33 @@
 /// - 1 <= `nums.length` <= 2 * 10<sup>4</sup>
 /// - 1 <= `nums[i]`, `k` <= `nums.length`
 pub struct Solution;
+
+use std::collections::hash_map::Entry::Occupied;
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn subarrays_with_k_distinct(nums: Vec<i32>, k: i32) -> i32 {
+        let sub_with_max_element_k = Self::subarrays_with_k_distinct_intr(&nums, k as usize);
+        let reduced_sub_with_max_k = Self::subarrays_with_k_distinct_intr(&nums, k as usize - 1);
+        sub_with_max_element_k - reduced_sub_with_max_k
+    }
+
+    fn subarrays_with_k_distinct_intr(nums: &Vec<i32>, k: usize) -> i32 {
+        let mut map = HashMap::new();
+        let (mut count, mut left) = (0, 0);
+
+        for right in 0..nums.len() {
+            map.entry(nums[right]).and_modify(|x| *x += 1).or_insert(1);
+            while map.len() > k {
+                if let Occupied(entry) = map.entry(nums[left]).and_modify(|x| *x -= 1) {
+                    if *entry.get() == 0 {
+                        entry.remove();
+                    }
+                }
+                left += 1;
+            }
+            count += right - left + 1;
+        }
+        count as i32
+    }
+}
