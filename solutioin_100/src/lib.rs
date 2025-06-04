7511a16f2706ac12effcdf8e9972af2877d4f6b7
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-06-04 22:32:36 +0200

    [100] Add solution: Same Tree

--- solutioin_100/src/lib.rs
+++ solutioin_100/src/lib.rs
@@ -24,12 +24,26 @@
 /// - -10<sup>4</sup> <= Node.val <= 10<sup>4</sup>
 pub struct Solution;
 
-use std::rc::Rc;
-use std::cell::RefCell;
 use binary_tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 impl Solution {
-    pub fn is_same_tree(_p: Option<Rc<RefCell<TreeNode>>>, _q: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        return false;
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (Some(a), Some(b)) => {
+                let a = a.borrow();
+                let b = b.borrow();
+
+                a.val == b.val
+                    && Self::is_same_tree(a.left.clone(), b.left.clone())
+                    && Self::is_same_tree(a.right.clone(), b.right.clone())
+            }
+            _ => false,
+        }
     }
 }
