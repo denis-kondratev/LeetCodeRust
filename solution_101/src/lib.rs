828828005852b0e178367d95a22fe60af0fee325
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-06-06 22:31:08 +0200

    [101] Change problem description: Symmetric Tree

--- solution_101/src/lib.rs
+++ solution_101/src/lib.rs
@@ -4,10 +4,12 @@
 /// (i.e., symmetric around its center).
 ///
 /// ## Example 1:
+/// ![image](https://assets.leetcode.com/uploads/2021/02/19/symtree1.jpg)
 /// - Input: `root` = `[1,2,2,3,4,4,3]`
 /// - Output: `true`
 ///
 /// ## Example 2:
+/// ![image](https://assets.leetcode.com/uploads/2021/02/19/symtree2.jpg)
 /// - Input: `root` = `[1,2,2,null,3,null,3]`
 /// - Output: `false`
 ///
