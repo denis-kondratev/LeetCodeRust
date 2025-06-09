3925b1ca0c1b4d6f8b0de4680783a596dd3afbac
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-06-09 19:18:18 +0200

    [104] Fix description: Maximum Depth of Binary

--- solution_104/src/lib.rs
+++ solution_104/src/lib.rs
@@ -6,6 +6,7 @@
 /// node down to the farthest leaf node.
 ///
 /// ## Example 1:
+/// ![image](https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg)
 /// - Input: `root = [3,9,20,null,null,15,7]`
 /// - Output: `3`
 ///
