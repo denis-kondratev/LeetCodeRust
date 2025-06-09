943a7bac2010e56a12d8eecc150e2b1c1687dadb
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-06-09 19:18:46 +0200

    [108] Add problem: Convert Sorted Array to Binary Search Tree

---
+++ solution_108/src/lib.rs
@@ -0,0 +1,35 @@
+/// # [108. Convert Sorted Array to Binary Search Tree](https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/)
+/// Given an integer array `nums` where the elements are sorted in ascending order,
+/// convert it to a height-balanced binary search tree.
+///
+/// A height-balanced binary tree is a binary tree in which the depth of the two subtrees
+/// of every node never differs by more than one.
+///
+/// ## Example 1:
+/// ![image](https://assets.leetcode.com/uploads/2021/02/18/btree1.jpg)
+/// - Input: `nums` = \[-10,-3,0,5,9]
+/// - Output: \[0,-3,9,-10,null,5]
+/// - Explanation: \[0,-10,5,null,-3,null,9] is also accepted:<br>
+/// ![image](https://assets.leetcode.com/uploads/2021/02/18/btree2.jpg)
+///
+/// ## Example 2:
+/// ![image](https://assets.leetcode.com/uploads/2021/02/18/btree.jpg)
+/// - Input: `nums` = \[1,3]
+/// - Output: \[3,1]
+/// - Explanation: \[1,null,3] and \[3,1] are both height-balanced BSTs.
+///
+/// ## Constraints:
+/// - 1 <= `nums.length` <= 10<sup>4</sup>
+/// - -10<sup>4</sup> <= `nums[i]` <= 10<sup>4</sup>
+/// - `nums` is sorted in a strictly increasing order.
+pub struct Solution;
+
+use binary_tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn sorted_array_to_bst(_nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        None
+    }
+}
