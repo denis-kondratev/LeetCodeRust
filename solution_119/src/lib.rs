e4e85b2ae1365399c12a3957e06bc7758576105b
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-06-15 16:38:00 +0200

    [119] Add solution: Pascal's Triangle II

--- solution_119/src/lib.rs
+++ solution_119/src/lib.rs
@@ -3,23 +3,41 @@
 /// Given an integer `rowIndex`, return the `rowIndex`<sup>th</sup> (0-indexed)
 /// row of the Pascal's triangle. In Pascal's triangle, each number is the sum of the
 /// two numbers directly above it as shown:
-/// 
+///
 /// ![image](https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif)
-/// 
+///
 /// ## Example 1:
 /// - Input: `rowIndex` = 3
 /// - Output: \[1, 3, 3, 1]
-/// 
+///
 /// ## Example 2:
 /// - Input: `rowIndex` = 0
 /// - Output: \[1]
-/// 
+///
 /// ## Example 3:
 /// - Input: `rowIndex` = 1
 /// - Output: \[1, 1]
-/// 
+///
 /// ## Constraints:
 /// - 0 < = `rowIndex` < = 33
-/// 
+///
 /// **Follow up:** Could you optimize your algorithm to use only `O(rowIndex)` extra space?
 pub struct Solution;
+
+impl Solution {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        let n = row_index as i64;
+        let mut row = Vec::with_capacity(row_index as usize + 1);
+
+        for k in 0..=n {
+            let Some(last) = row.last().copied() else {
+                row.push(1);
+                continue;
+            };
+
+            row.push((last as i64 * (n - k + 1) / k) as i32);
+        }
+
+        row
+    }
+}
