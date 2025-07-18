a9bab6ed97ab3aaaf27404e82b3d72738c1c1748
Denis Kondratev <53820149+denis-kondratev@users.noreply.github.com>
2025-07-18 10:48:26 +0200

    [59] Add solution: Spiral Matrix II

--- solution_59/src/lib.rs
+++ solution_59/src/lib.rs
@@ -15,3 +15,42 @@
 /// ## Constraints:
 /// - 1 <= n <= 20
 pub struct Solution;
+
+impl Solution {
+    pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
+        let mut result = vec![vec![0; n as usize]; n as usize];
+        let (mut min, mut max) = (0, n as usize - 1);
+        let mut number = 1;
+
+        while min < max {
+            for i in min..=max {
+                result[min][i] = number;
+                number += 1;
+            }
+
+            for i in min + 1..=max {
+                result[i][max] = number;
+                number += 1;
+            }
+
+            for i in (min..max).rev() {
+                result[max][i] = number;
+                number += 1;
+            }
+
+            for i in (min + 1..max).rev() {
+                result[i][min] = number;
+                number += 1;
+            }
+
+            min += 1;
+            max -= 1;
+        }
+
+        if min == max {
+            result[min][min] = number;
+        }
+
+        result
+    }
+}
